75adf1276ddd51764884e484927207c7ca08d7ec
sheng153 <nicolas.sabio@gmail.com>
2023-10-31 19:21:30 -0300

    aburrrrrrriiiiiiiiddddddddoooooooo

--- src/main/java/com/mycompany/programacion_2/Empleado.java
+++ src/main/java/com/mycompany/programacion_2/Empleado.java
@@ -3,10 +3,20 @@ package com.mycompany.programacion_2;
 import java.util.Scanner;
 
 public class Empleado extends Persona {
+    private float sueldo;
     
     public Empleado(Scanner scan) {
         super(scan);
-        
+        set_sueldo();
+    }
+    
+    public final void set_sueldo() {
+        System.out.println("Ingrese el sueldo del empleado: ");
+        this.sueldo = scan.nextInt();
+    }
+    
+    public float get_sueldo() {
+        return sueldo;
     }
     
     @Override
--- src/main/java/com/mycompany/programacion_2/Persona.java
+++ src/main/java/com/mycompany/programacion_2/Persona.java
@@ -3,11 +3,21 @@ package com.mycompany.programacion_2;
 import java.util.Scanner;
 
 public abstract class Persona {
-    int dni;
+    protected int dni;
+    protected String nombre;
+    protected String apellido;
+    protected final Scanner scan;
     
     public abstract void print();
     
     protected Persona(Scanner scan) {
+        this.scan = scan;
+        set_dni();
+        set_name();
+        set_last_name();
+    }
+    
+    public final void set_dni() {
         int tdni;
         while(true) {
             System.out.println("Ingrese el DNI: ");
@@ -15,6 +25,16 @@ public abstract class Persona {
             if (tdni > 9999999 && tdni < 100000000) { this.dni = tdni; break; }
             System.out.println("Ingrese un documento valido.");
         }
+    }
+    
+    public final void set_name() {
+        System.out.println("Ingrese el nombre: ");
+        nombre = scan.nextLine();
+    }
+    
+    public final void set_last_name() {
+        System.out.println("Ingrese el apellido: ");
+        apellido = scan.nextLine();
     }
     
     public boolean dni(int n) {
